9fa5a85bce9dc8a77455a38e08a4bc58a7733459
zdoroven <45197696+zdoroven@users.noreply.github.com>
2019-04-30 14:46:15 +0300

    Update rh.h

--- tests/rh.h
+++ tests/rh.h
@@ -64,4 +64,35 @@ TEST(rh, test2) {
         FAIL();
     }
 }
+
+TEST(rh, test3) {
+
+    FILE *outputFile;
+    TRAVIS ? outputFile = fopen("tests/output/output5.txt", "wb") : outputFile = fopen("C:/work/testing/testeditor/tests/output/output5.txt", "wb");
+    if (outputFile == NULL) {
+        printf("Cannot open file for output");
+        FAIL();
+    }
+    int oldstdOut = changeStream(outputFile);
+
+    text txt = create_text();
+    char inFile[MAXLINE];
+    TRAVIS ? strncpy(inFile, "tests/input/input5.txt", MAXLINE) : strncpy(inFile, "C:/work/testing/testeditor/tests/input/input5.txt", MAXLINE);
+    load(txt, inFile);
+
+    rh(txt);
+    show(txt);
+
+    returnStream(outputFile, oldstdOut);
+    FILE *expectedData;
+    TRAVIS ? expectedData = fopen("tests/expected/expected5.txt", "r") : expectedData = fopen("C:/work/testing/testeditor/tests/expected/expected5.txt", "r");
+    FILE *outputData;
+    TRAVIS ? outputData = fopen("tests/output/output5.txt", "r") : outputData = fopen("C:/work/testing/testeditor/tests/output/output5.txt", "r");
+
+    if (executeTest(expectedData, outputData) == 1) {
+        SUCCEED();
+    } else {
+        FAIL();
+    }
+}
 #endif // RH_H
